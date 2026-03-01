6f289e215e1e8f03f004990ed2cd66fb1efaa7f8
Avi Vahl <avi@vahl.co.il>
2026-03-01 20:58:37 +0200

    chore(deps-dev): eslint@10, latest others - using eslint-plugin-react-hooks@7.1.0-canary-e0cc7202-20260227 for compatibility with v10 - regenerate lock file

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@
 import pluginJs from '@eslint/js';
 import configPrettier from 'eslint-config-prettier';
 import pluginNoOnlyTests from 'eslint-plugin-no-only-tests';
-import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import pluginTypescript from 'typescript-eslint';
@@ -11,16 +10,12 @@ import pluginTypescript from 'typescript-eslint';
 export default defineConfig([
     globalIgnores(['**/dist/', 'packages/color-scheme/playwright.config.ts']),
     pluginJs.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    pluginReact.configs.flat['jsx-runtime'],
-    { settings: { react: { version: 'detect' } } },
     pluginReactHooks.configs.flat.recommended,
     { plugins: { 'no-only-tests': pluginNoOnlyTests } },
     {
         rules: {
             'no-only-tests/no-only-tests': 'error',
             'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-            'react/prop-types': 'off',
             'react-hooks/exhaustive-deps': 'error',
         },
     },
