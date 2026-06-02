88b1d77c8ddd3b3533e372d555943feaf3fb9268
Ido Rosenthal <idoros@gmail.com>
2026-06-02 17:53:49 +0300

    feat: expose "@dazl/dev/plugins"

---
+++ packages/dev/src/plugins/index.ts
@@ -0,0 +1,13 @@
+export { parseDevRoutePath } from './url.ts';
+export { defineApi, type ApiHandler } from './route-api.ts';
+export {
+    clientAction,
+    clientLoader,
+    definePluginView,
+    HydrateFallback,
+    PluginView,
+    type PluginViewComponentProps,
+    type PluginViewDefinition,
+    type PluginViewLoaderArgs,
+    type PluginViewProps,
+} from './plugin-view.tsx';
